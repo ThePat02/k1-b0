5270d2e9805a54165015f6ea9bf30c243e5fd404
Pat <27511950+ThePat02@users.noreply.github.com>
2023-01-17 21:41:08 +0000

    first commit

---
+++ bot.py
@@ -0,0 +1,23 @@
+
+# This example requires the 'message_content' intent.
+
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if "r6" in message.content.lower():
+        await message.channel.send('No.')
+
+client.run('MTA1NzQzMTUxNzI1MDQ3Mzk5NA.GF8Piy.0d85mzmOqE6rPd0YcZLNQeJysjaiLXz1wB2MgU')
