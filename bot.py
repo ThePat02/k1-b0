965cee56d07bd0febe884569f25bef56fef48a3c
Pat <27511950+ThePat02@users.noreply.github.com>
2023-06-03 12:26:29 +0000

    add: basic HTTP request

--- bot.py
+++ bot.py
@@ -1,24 +1,41 @@
-
-# This example requires the 'message_content' intent.
-
 import discord
+from discord.ext import commands
+
+import requests
+import json
+
+COMMAND_PREFIX = '?'
+URL = "https://api.henrikdev.xyz/valorant/v1/"
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+class ValorantUser:
+    """A valornt user object that contains the user's information"""
+    def __init__(self, name, tag, card, level):
+        self.name = name
+        self.tag = tag
+        self.card = card
+        self.level = level
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
-    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Wingman eat your brains"))
+bot = commands.Bot(command_prefix=COMMAND_PREFIX, intents=intents)
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@bot.command()
+async def valo(ctx, user):
+    """TODO WRITE DOCSTRING"""
+    
+    #Split the user by the # to get the username and discriminator
+    user = user.split('#')
+    username = user[0]
+    discriminator = user[1]
+    
+    response_user = requests.get(URL + "account/" + username + "/" + discriminator)
+    response_user = response_user.json()
+    
+    player = ValorantUser(response_user['data']['name'], response_user['data']['tag'], response_user['data']['card']['small'], response_user['data']['account_level'])
+    
+    await ctx.send(player.name + "#" + player.tag +  " Level " + str(player.level))
+    await ctx.send(player.card)
+    
 
-    if "r6" in message.content.lower():
-        await message.channel.send('No.')
-
-client.run('MTA1NzQzMTUxNzI1MDQ3Mzk5NA.GF8Piy.0d85mzmOqE6rPd0YcZLNQeJysjaiLXz1wB2MgU')
+bot.run('MTA1NzQzMTUxNzI1MDQ3Mzk5NA.GF8Piy.0d85mzmOqE6rPd0YcZLNQeJysjaiLXz1wB2MgU')
