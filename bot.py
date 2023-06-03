12c8859cf4e09a9f0d61f091efd7acd3446e9ec3
Pat <27511950+ThePat02@users.noreply.github.com>
2023-06-03 23:15:32 +0000

    add: Status watching Wingman

--- bot.py
+++ bot.py
@@ -5,10 +5,10 @@
 
 # Import discord library
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 # Import libraries
-import requests
+import datetime
 
 # Import cogs
 from cogs.greetings import Greetings
@@ -43,9 +43,40 @@ async def on_ready():
     utils.print_line()
     await bot.add_cog(Greetings(bot))
     await bot.add_cog(Valorant(bot))
-    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Wingman sleep"))
     print("Beep-bop! K1-B0 is ready to roll!")
     utils.print_line()
+
+    change_status.start()
+
+@tasks.loop(minutes=30)
+async def change_status():
+    """Changes the bot's status"""
+    # Get time
+    now = datetime.datetime.now() 
+    status = "Wingman do stuff"
+
+    hour = now.hour + 2
+    if hour >= 24: hour -= 24
+
+    if hour >= 6 and hour < 9:
+        status = "Wingman wake up"
+    elif hour >= 9 and hour < 12:
+        status = "Wingman plant the spike"
+    elif hour >= 12 and hour < 15:
+        status = "Wingman eat a snack"
+    elif hour >= 15 and hour < 18:
+        status = "Wingman play with Dizzy"
+    elif hour >= 18 and hour < 20:
+        status = "Wingman eat dinner"
+    elif hour >= 20 and hour < 22:
+        status = "Wingman working out"
+    elif hour >= 22 and hour < 24:
+        status = "Wingman sleep"
+    elif hour >= 0 and hour < 6:
+        status = "Wingman sleeping deeply"
+
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=status))
+
 
 # -----------------------------------------------------------------------------
 # Run K1-B0
